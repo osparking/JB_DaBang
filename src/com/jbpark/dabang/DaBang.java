b1a6f4a19505d2d7d0c5cd89a58d2a4217aee876
Park, JongBum <jbpark03@gmail.com>
2021-05-21 17:31:25 +0900

    JB DaBang Opening commit

---
+++ src/com/jbpark/dabang/DaBang.java
@@ -0,0 +1,10 @@
+package com.jbpark.dabang;
+
+public class DaBang {
+
+	public static void main(String[] args) {
+		TeaType tea = TeaType.보성녹차;
+		System.out.println("JB 다방에 환영합니다");
+		System.out.println(TeaType.율무차);
+	}
+}
---
+++ src/com/jbpark/dabang/TeaType.java
@@ -0,0 +1,5 @@
+package com.jbpark.dabang;
+
+public enum TeaType {
+	감잎차, 보성녹차, 율무차
+}
