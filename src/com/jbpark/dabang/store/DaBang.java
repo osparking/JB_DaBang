8ffcdc84b334b80b8886cb94dd617ed90495f54c
Park, JongBum <jbpark03@gmail.com>
2021-05-27 15:52:19 +0900

    for menu input, switch used

--- src/com/jbpark/dabang/store/DaBang.java
+++ src/com/jbpark/dabang/store/DaBang.java
@@ -21,27 +21,39 @@ public class DaBang {
 		/**
 		 * 나중에 Enum to List 사용하여 깔끔하게 정리
 		 */
-		String menu = String.join("/", 
-				TeaType.감잎차.toString(), 
-				TeaType.보성녹차.toString(), 
-				TeaType.율무차.toString() );
-		
+		String menu = String.join("/", TeaType.감잎차.toString() + (TeaType.감잎차.ordinal() + 1),
+				TeaType.보성녹차.toString() + (TeaType.보성녹차.ordinal() + 1),
+				TeaType.율무차.toString() + (TeaType.율무차.ordinal() + 1));
+
 		System.out.println(menu);
 		int idx = LocalDate.now().getDayOfYear() % teaTypes.length;
-//		teaTypes // 배열 학습 후 완성
-		String weekDay = LocalDateTime.now().format(
-				DateTimeFormatter.ofPattern("E") // E => Weekday
+
+		String weekDay = LocalDateTime.now().format(DateTimeFormatter.ofPattern("E") // E => Weekday
 				.withLocale(Locale.KOREAN));
 		System.out.println(weekDay + "요일 스페셜: " + teaTypes[idx]);
-//		TeaType.감잎차.ordinal()
-		
+
 		System.out.println("원하는 차 종류를 입력: ");
-		
-		Scanner scanner = new Scanner(System.in);		
+
+		Scanner scanner = new Scanner(System.in);
+
 		int 제품번호 = scanner.nextInt();
-		System.out.println("제품번호 " + 제품번호 + "를 선택하셨습니다.");
+
+		TeaType teaType = TeaType.values()[제품번호 - 1];
+		switch (teaType) {
+		case 감잎차:
+			System.out.println("감잎차를 선택하셨습니다.");
+			break;
+		case 보성녹차:
+			System.out.println("보성녹차를 선택하셨습니다.");
+			break;
+		case 율무차:
+			System.out.println("율무차를 선택하셨습니다.");
+			break;
+		default:
+			break;
+		}
 		scanner.close();
-		
-		return ;
+
+		return;
 	}
 }
