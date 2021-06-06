e924f481376232507a143fc64932c043d2f6cac5
Park,JongBum <jbpark03@gmail.com>
2021-06-06 14:37:16 +0900

    DaBang DB created and granted to myself user

---
+++ resources/db_dabang_ddl.sql
@@ -0,0 +1,17 @@
+GRANT Alter ON jb_dabang.* TO 'myself'@'%';
+GRANT Create ON jb_dabang.* TO 'myself'@'%';
+GRANT Create view ON jb_dabang.* TO 'myself'@'%';
+GRANT Delete ON jb_dabang.* TO 'myself'@'%';
+GRANT Delete history ON jb_dabang.* TO 'myself'@'%';
+GRANT Drop ON jb_dabang.* TO 'myself'@'%';
+GRANT Grant option ON jb_dabang.* TO 'myself'@'%';
+GRANT Index ON jb_dabang.* TO 'myself'@'%';
+GRANT Insert ON jb_dabang.* TO 'myself'@'%';
+GRANT References ON jb_dabang.* TO 'myself'@'%';
+GRANT Select ON jb_dabang.* TO 'myself'@'%';
+GRANT Show view ON jb_dabang.* TO 'myself'@'%';
+GRANT Trigger ON jb_dabang.* TO 'myself'@'%';
+GRANT Update ON jb_dabang.* TO 'myself'@'%';
+
+/* 일단 데이터베이스를 root 로써 생성 */
+CREATE DATABASE `jb_dabang` /*!40100 DEFAULT CHARACTER SET utf8 */;
