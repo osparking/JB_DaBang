c55ffcae4e9be8b1f6b5a7534b7762e14814cdd8
Park, JongBum <jbpark03@gmail.com>
2021-06-07 11:43:22 +0900

    전통차 테이블 스키마(3컬럼) 생성

--- resources/db_dabang_ddl.sql
+++ resources/db_dabang_ddl.sql
@@ -1,3 +1,12 @@
+-- jb_dabang.전통차 definition
+
+CREATE TABLE `전통차` (
+  `tea_id` char(5) NOT NULL,
+  `tea_name` varchar(20) NOT NULL COMMENT '차 이름(예, 질경이차)',
+  `tea_description` varchar(200) DEFAULT NULL COMMENT '차 특성 등을 말로 설명',
+  PRIMARY KEY (`tea_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 GRANT Alter ON jb_dabang.* TO 'myself'@'%';
 GRANT Create ON jb_dabang.* TO 'myself'@'%';
 GRANT Create view ON jb_dabang.* TO 'myself'@'%';
