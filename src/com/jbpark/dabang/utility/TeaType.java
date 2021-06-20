4ae28d1f06c3bc351668966f31592ef240078284
Park,JongBum <jbpark03@gmail.com>
2021-06-20 11:06:37 +0900

    TeaType enum augmented with short name

--- src/com/jbpark/dabang/utility/TeaType.java
+++ src/com/jbpark/dabang/utility/TeaType.java
@@ -2,9 +2,25 @@ package com.jbpark.dabang.utility;
 
 /**
  * 한국 전통차(예, {@code 보성녹차})의 종류 열거형
+ * 
  * @author Park, JongBum
  *
  */
 public enum TeaType {
-	감잎차, 보성녹차, 율무차
+	감잎차("ㄱ"), 보성녹차("ㅂ"), 율무차("ㅇ");
+
+	private String 단축명;
+
+	private TeaType(String 단축명) {
+		this.단축명 = 단축명;
+	}
+
+	public String get단축명() {
+		return 단축명;
+	}
+	
+	@Override
+	public String toString() {
+		return name() + "(" + get단축명() + ")";
+	}
 }
