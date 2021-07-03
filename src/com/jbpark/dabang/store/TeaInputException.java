c075c48c8c2074d824bde389391b0baebcdbf921
Park,JongBum <jbpark03@gmail.com>
2021-07-03 11:35:37 +0900

    Exception for wrong tea type input

---
+++ src/com/jbpark/dabang/store/TeaInputException.java
@@ -0,0 +1,16 @@
+package com.jbpark.dabang.store;
+
+import java.io.IOException;
+
+public class TeaInputException extends IOException {
+
+	public TeaInputException(String selection) {
+		super(selection);
+	}
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 5784146167775267154L;
+
+}
